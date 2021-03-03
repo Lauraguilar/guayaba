d279b3d48328f9ac1e04b9400801d9805c86c041
Laura Aguilar <laura.aguilar90@gmail.com>
2021-03-03 20:14:42 -0300

    se agrega boton Vaciar Carrito

--- src/components/VistaCarrito.jsx
+++ src/components/VistaCarrito.jsx
@@ -17,32 +17,43 @@ const VistaCarrito = () => {
             setProductos(JSON.parse(localStorage.getItem('productos')));
         } else {
             setProductos(data.items);   
-        }
+        } 
 
     }, [data.items])
+
+    console.log(data)
+    console.log(productos)
 
     return (
         <div className='vistaCarrito container'>
             <h1>Tu Carrito</h1>
+            {
+                data.cantidad !==0 ? 
+                    <>
+                    <ul>
+                        {
+                            productos.map((item, index) => (
+                                <li className='row item' key={index}>
+                                    <img src={item.item.foto} alt="" className='col'/>
+                                    <div className='col'>
+                                        <h2>{item.item.titulo}</h2>
+                                        <p>Cantidad: {item.cantidad}</p>
+                                        <p>Precio por unidad: <strong>${item.item.valor}</strong></p>
+                                        <p>Precio total: <strong>${item.item.valor * item.cantidad}</strong></p>
+                                    </div>
+                                </li>
+                                ))
+                        }
+                    </ul>
+                    <div className='d-flex justify-content-center'>    
+                        <Link className='btn btn-primary' to='/checkout'>Finalizar Compra</Link>
+                    </div>
+                    </>
+                :
+                <p>Tienes que agregar productos a tu carrito</p>
+            }
 
-            <ul>
-                {
-                    productos.map((item, index) => (
-                        <li className='row item' key={index}>
-                            <img src={item.item.foto} alt="" className='col'/>
-                            <div className='col'>
-                                <h2>{item.item.titulo}</h2>
-                                <p>Cantidad: {item.cantidad}</p>
-                                <p>Precio por unidad: <strong>${item.item.valor}</strong></p>
-                                <p>Precio total: <strong>${item.item.valor * item.cantidad}</strong></p>
-                            </div>
-                        </li>
-                    ))
-                }
-            </ul>
-            <div className='d-flex justify-content-center'>    
-                <Link className='btn btn-primary' to='/checkout'>Finalizar Compra</Link>
-            </div>
+
         </div>
     )
 }
--- src/components/WidgetCarrito.jsx
+++ src/components/WidgetCarrito.jsx
@@ -1,5 +1,5 @@
 import '../estilos/WidgetCarrito.css';
-import {useContext} from 'react';
+import {useContext, useEffect} from 'react';
 import {Store} from '../store';
 import {CgClose} from 'react-icons/cg';
 import {Link} from 'react-router-dom';
@@ -8,30 +8,44 @@ const WidgetCarrito = ({mostrar, action}) => {
 
     const [data, setData] = useContext(Store);
 
+    const vaciarCarrito = () => {
+        setData ({
+            items: [],
+            cantidad: 0,
+            precioTotal: 0,
+        })
+    }
+    
     return (
         <div className={`widgetCarrito ${mostrar ? 'open' : 'close'}`}>
             <div className='contenido container'>
                 <button className='btn btn-link'><CgClose onClick={action}/></button>
                 <h4 className='d-flex justify-content-center'>Tu Carrito</h4>
-                
-                {
-                    data.cantidad !== 0 ?
-                        data.items.map((item, index) => 
-                        <div key={index}>
-                            <div className='row'>
-                                <div className='col'>
-                                    <p>{item.item.titulo}</p>
-                                </div>
-                                <div className='col'>
-                                    <p className='d-inline-block'>x{item.cantidad}</p>
-                                </div>
-                            </div>
-                        </div>)
-                    :
-                    <p>No hay productos en tu carrito</p>
-                }
+                <ul>
+                    {
+                        data.cantidad !== 0 ?
+                            data.items.map((item, index) => (
+                                <li className='row' key={index}>
+                                    <div className='col-6'>
+                                        <p className='d-inline-block'>{item.item.titulo}</p>
+                                    </div>
+                                    <div className='col-3'>
+                                        <p className='d-inline-block'>(x{item.cantidad})</p>
+                                    </div>
+                                    <div className='col-3'>
+                                        <p className='d-inline-block'>${item.cantidad*item.item.valor}</p>
+                                    </div>
+                                </li>
+                            ))
+                        :
+                        <p>No hay productos en tu carrito</p>
+                    }
+                </ul>
                 <h6>Total: ${data.precioTotal}</h6>
-                <Link className='btn btn-outline-primary' to='/carrito'>Ver</Link>
+                <button onClick={vaciarCarrito}>Vaciar Carrito</button>
+                <div className='row ver'>
+                    <Link className='btn btn-primary' to='/carrito'>Ver</Link>
+                </div>
             </div>
         </div>
     )
